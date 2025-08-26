a5c406b034203b93f4edf392c6b96eae324e5d52
rj7075 <128252566+rj7075@users.noreply.github.com>
2025-08-26 22:03:55 +0530

    Update Therapists.jsx

--- client/src/pages/Therapists.jsx
+++ client/src/pages/Therapists.jsx
@@ -9,7 +9,7 @@ const TherapistSection = () => {
     const fetchTherapists = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:5000/api/therapists"
+          "https://appointment-booking-backend-mw2h.onrender.com/api/therapists"
         );
         setTherapists(res.data);
       } catch (err) {
