15bbe04891fbd37e92da1c893aeb601f33429274
rj7075 <128252566+rj7075@users.noreply.github.com>
2025-08-26 21:56:40 +0530

    Update server.js
    some message added

--- server/server.js
+++ server/server.js
@@ -36,3 +36,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+
