3dca8ccbad62dc4e0f723c0fe0a76ab8224c2189
rj7075 <128252566+rj7075@users.noreply.github.com>
2025-08-26 22:02:52 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     try {
       // Step 1: Login request
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://appointment-booking-backend-mw2h.onrender.com/api/auth/login",
         formData
       );
       const token = res.data.token;
