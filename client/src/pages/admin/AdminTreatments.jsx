7908b0c29473345ae6b69df059bb7f9c626c4303
rj7075 <rj7075yadav.com>
2025-09-13 21:54:09 +0530

    file updated

--- client/src/pages/admin/AdminTreatments.jsx
+++ client/src/pages/admin/AdminTreatments.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+
+
 
 const AdminTreatments = () => {
   const [treatments, setTreatments] = useState([]);
